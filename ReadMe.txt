e6d6fa0e8cdaf519be126d26f23515ede9e90bca
Klaz <facumierke@gmail.com>
2023-10-16 20:01:40 -0300

    V 0.1.1

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,12 @@
 Gestor de Contraseñas
 
+V 0.1.1
+
+* Se modifico la forma de entrada de datos para poder almacenar mas de un tipo de dato.
+* Se permite la consulta de los datos a traves de perfiles, que toman como referencia para mostrar los datos el nombre del primer dato introducido.
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 V 0.1
 
 * Interfaz basica creada, se manipula la aplicacion mediante la consola.
--- mainApp.py
+++ mainApp.py
@@ -1,29 +1,37 @@
 import os
 
-def guardar_datos():
+def guardar_perfil():
     nombre = input("Por favor, introduce tu nombre: ")
-    with open('datos.txt', 'w') as archivo:
-        archivo.write(nombre)
+    fecha_nacimiento = input("Introduce tu fecha de nacimiento (dd/mm/aaaa): ")
+    contraseña = input("Crea una contraseña: ")
 
-def recuperar_datos():
-    if os.path.exists('datos.txt'):
-        with open('datos.txt', 'r') as archivo:
-            nombre = archivo.read()
-            print(f"Hola, {nombre}!")
-    else:
-        print("No se encontraron datos guardados.")
+    with open('perfiles.txt', 'a') as archivo:
+        archivo.write(f"{nombre},{fecha_nacimiento},{contraseña}\n")
+    print(f"Perfil de {nombre} guardado correctamente.")
+
+def recuperar_perfil():
+    nombre = input("Por favor, introduce tu nombre: ")
+    with open('perfiles.txt', 'r') as archivo:
+        for linea in archivo:
+            datos = linea.strip().split(',')
+            if datos[0] == nombre:
+                print(f"Nombre: {datos[0]}")
+                print(f"Fecha de Nacimiento: {datos[1]}")
+                print(f"Contraseña: {datos[2]}")
+                return
+        print(f"No se encontró un perfil para {nombre}.")
 
 def main():
-    print("Bienvenido a la aplicación de guardado de datos.")
-    print("1. Guardar datos")
-    print("2. Recuperar datos")
+    print("Bienvenido a la aplicación de perfiles.")
+    print("1. Guardar perfil")
+    print("2. Recuperar perfil")
 
     opcion = input("Por favor, elige una opción (1 o 2): ")
 
     if opcion == '1':
-        guardar_datos()
+        guardar_perfil()
     elif opcion == '2':
-        recuperar_datos()
+        recuperar_perfil()
     else:
         print("Opción inválida. Por favor, elige 1 o 2.")
 
